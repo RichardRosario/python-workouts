606e4335fbc178dd57ff7e1f98934df2c47f545a
Richard Rosario <tech2js@gmail.com>
2020-08-04 21:12:55 +0800

    automate prompts

--- break_time.py
+++ break_time.py
@@ -1,2 +1,11 @@
 import webbrowser
-webbrowser.open("https://www.youtube.com/watch?v=EYyarcp5LtU")
+from time import sleep
+
+breaks = 3
+break_count = 0
+
+while(break_count < breaks):
+    sleep(10)
+    print(webbrowser.open("https://www.youtube.com/watch?v=EYyarcp5LtU"))
+    break_count = break_count + 1
+print("promted 3 times")

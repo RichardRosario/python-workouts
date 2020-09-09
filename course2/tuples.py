30392cb53d746f5dbb7b9f27886f6a0a94511d29
Richard Rosario <tech2js@gmail.com>
2020-09-09 13:31:04 +0800

    python workouts today.. more tuples and dictionary

--- course2/tuples.py
+++ course2/tuples.py
@@ -41,3 +41,14 @@ for item in enumerate(fruits):
 fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
 for idx, fruit in enumerate(fruits):
     print(idx, fruit)
+
+
+# unpacking === With only one line of code, assign the variables city, country, and year to the values of the tuple olymp.
+olymp = ('Rio', 'Brazil', 2016)
+(city, country, year) = olymp
+
+# unpacking with a function.. Define a function called info with the following required parameters: name, age, birth_year, year_in_college, and hometown. The function should return a tuple that contains all the inputted information.
+
+
+def info(name, age, birth_year, year_in_college, hometown):
+    return (name, age, birth_year, year_in_college, hometown)
--- practice.py
+++ practice.py
@@ -17,9 +17,24 @@ def adding(x):
 # adding(4)
 
 
-def producing(x):
-    z = x * y
-    return z
+# def producing(x):
+#     z = x * y
+#     return z
 
 
-print(producing(adding(4)))
+# print(producing(adding(4)))
+
+# ============
+# You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide function within the sum function. Do not worry about decimals.
+
+
+def divide(x):
+    return x / 2
+
+
+def sum(y):
+    return divide(y) + 6
+
+
+print(sum(4))
+print(divide(3))

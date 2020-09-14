e733392033c41e4d72e8c8719de1951bb6e5b331
Richard Rosario <tech2js@gmail.com>
2020-09-14 13:27:47 +0800

    arrays workout

---
+++ practice1.py
@@ -0,0 +1,11 @@
+import numpy
+
+# =================== You are given two arrays:  and .
+# Your task is to compute their inner and outer product.
+# A = numpy.array(input().split(), int)
+# B = numpy.array(input().split(), int)
+
+# print(numpy.inner(A, B))
+# print(numpy.outer(A, B))
+
+# ==================================

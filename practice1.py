10852ddca380407438f4aa5fff28ad3a2b69d117
Richard Rosario <tech2js@gmail.com>
2020-09-15 23:52:52 +0800

    workout on finditer

--- practice1.py
+++ practice1.py
@@ -33,15 +33,15 @@ from itertools import combinations, product
 # You have to pick one element from each list so that the value from the equation below is maximized:
 # The first line contains 2 space separated integers K and M.
 # The next  lines each contains an integer N, denoting the number of elements in the  list, followed by  space separated integers denoting the elements in the list.
-K, M = map(int, input().split())
+# K, M = map(int, input().split())
 # N = (list(map(int, input().split())) for _ in range(1, K)) -- first option
 # N = (list(map(int, input().split()))[1:] for _ in range(K)) -- best option
 
 # results = map(lambda x: sum(i**2 for i in x) % M, product(*N))
 
 # version 2
-print(max(sum(j) % M for j in product(
-    *((int(i) ** 2 for i in input().split()[1:]) for _ in range(K)))))
+# print(max(sum(j) % M for j in product(
+#     *((int(i) ** 2 for i in input().split()[1:]) for _ in range(K)))))
 
 # print(max(results))
 
@@ -50,3 +50,13 @@ print(max(sum(j) % M for j in product(
 # c = "qwrtypsdfghjklzxcvbnm"
 # print(*re.findall("(?=[%s]([%s]{2,})[%s])" %
 #                   (c, v, c), input(), re.I) or [-1], sep="\n")
+
+# =======================
+
+# s = 'halhsdflasasdf'
+# k = 'alsdlajsgjpirpnapv'
+# if k in s:
+#     print(*[(i.start(), (i.start()+len(k)-1))
+#             for i in re.finditer(r'(?={})'.format(k), s)], sep='\n')
+# else:
+#     print('(-1, -1)')

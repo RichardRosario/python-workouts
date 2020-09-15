070147ed6e8903ad4bf981c8d14adf5b5edcb8a5
Richard Rosario <tech2js@gmail.com>
2020-09-15 11:22:02 +0800

    workout on getting the maximum value

--- practice1.py
+++ practice1.py
@@ -11,3 +11,31 @@ import numpy
 # print(numpy.outer(A, B))
 
 # ==================================
+
+from itertools import combinations, product
+
+N = int(input())
+S = input().split()
+K = int(input())
+
+num = 0
+den = 0
+
+for c in combinations(S, K):
+    den += 1
+    num += 'a' in c
+
+# print(float(num)/den)
+
+# =============================
+# You are given a function . You are also given  lists. The  list consists of  elements.
+# You have to pick one element from each list so that the value from the equation below is maximized:
+# The first line contains 2 space separated integers K and M.
+# The next  lines each contains an integer N, denoting the number of elements in the  list, followed by  space separated integers denoting the elements in the list.
+K, M = map(int, input().split())
+#N = (list(map(int, input().split())) for _ in range(1, K))
+N = (list(map(int, input().split()))[1:] for _ in range(K))
+
+results = map(lambda x: sum(i**2 for i in x) % M, product(*N))
+
+# print(max(results))

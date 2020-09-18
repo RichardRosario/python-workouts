8936c8cdcc7e0a2fb1dc0dcb779e829d1f43e08c
Richard Rosario <tech2js@gmail.com>
2020-09-18 10:52:04 +0800

    checking a valid credit card

--- practice1.py
+++ practice1.py
@@ -185,13 +185,13 @@ from html.parser import HTMLParser
 # [print('Valid') if re.match(r'^(?!.*(.).*\1)(?=(?:.*[A-Z]){2,})(?=(?:.*\d){3,})[a-zA-Z0-9]{10}$', input(
 # )) else print('Invalid') for _ in range(int(input()))]
 
-N = input('B1CD102354')
-for _ in range(int(N)):
-    if re.match(r'^(?!.*(.).*\1)(?=(?:.*[A-Z]){2,})(?=(?:.*\d){3,})[a-zA-Z0-9]{10}$', input(
-    )):
-        print('Valid')
-    else:
-        print('Invalid')
+# N = input('B1CD102354')
+# for _ in range(int(N)):
+#     if re.match(r'^(?!.*(.).*\1)(?=(?:.*[A-Z]){2,})(?=(?:.*\d){3,})[a-zA-Z0-9]{10}$', input(
+#     )):
+#         print('Valid')
+#     else:
+#         print('Invalid')
 
 # Explanation:
 
@@ -201,3 +201,26 @@ for _ in range(int(N)):
 # [a-zA-Z0-9]{10} checks exactly 10 alphanumeric characters.
 
 # ================================
+# You and Fredrick are good friends. Yesterday, Fredrick received  credit cards from ABCD Bank. He wants to verify whether his credit card numbers are valid or not. You happen to be great at regex so he is asking for your help!
+
+# A valid credit card from ABCD Bank has the following characteristics:
+
+# ► It must start with a ,  or .
+# ► It must contain exactly  digits.
+# ► It must only consist of digits (-).
+# ► It may have digits in groups of , separated by one hyphen "-".
+# ► It must NOT use any other separator like ' ' , '_', etc.
+# ► It must NOT have  or more consecutive repeated digits.
+cc_test = re.compile(
+    r"^"
+    r"(?!.*(\d)(-?\1){3})"
+    r"[456]"
+    r"\d{3}"
+    r"(?:-?\d{4}){3}"
+    r"$")
+
+for _ in range(int(input().strip())):
+    if cc_test.search(input().strip()):
+        print('Valid')
+    else:
+        print("Invalid")

867c207b1adc8e24c56708374064cd0cdd62e0e0
Richard Rosario <tech2js@gmail.com>
2020-09-21 22:31:01 +0800

    symmetric difference operations

--- practice2.py
+++ practice2.py
@@ -88,8 +88,18 @@ import re
 # The students of District College have subscriptions to English and French newspapers. Some students have subscribed only to English, some have subscribed to only French and some have subscribed to both newspapers.
 
 # You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper. The same student could be in both sets. Your task is to find the total number of students who have subscribed to at least one newspaper.
-n = input()
-set_n = set(map(int, input().split()))
-b = input()
-set_b = set(map(int, input().split()))
+# n = input()
+# set_n = set(map(int, input().split()))
+# b = input()
+# set_b = set(map(int, input().split()))
 # print(len(set_n.union(set_b)))
+# ========================
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to both newspapers.
+# num1, st1, num2, st2 = (set(input().split()) for i in range(4))
+# print(len(st1.intersection(st2)))
+# ======================
+n1 = int(input())
+set1 = set(map(int, input().split()))
+n2 = int(input())
+set2 = set(map(int, input().split()))
+# print(len(set1-set2))

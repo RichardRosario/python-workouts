5f5e1ee77008d464c20a29d650426067e63fa48c
Richard Rosario <tech2js@gmail.com>
2020-09-22 10:25:10 +0800

    workout on union,intersection,difference,mutations

--- practice2.py
+++ practice2.py
@@ -103,3 +103,12 @@ set1 = set(map(int, input().split()))
 n2 = int(input())
 set2 = set(map(int, input().split()))
 # print(len(set1-set2))
+
+# =======================
+# You are given two sets of student roll numbers. One set has subscribed to the English newspaper, and one set has subscribed to the French newspaper. Your task is to find the total number of students who have subscribed to either the English or the French newspaper but not both.
+n1, eng = int(input()), set(input().split())
+
+n2, fr = int(input()), set(input().split())
+
+result = eng.union(fr) - eng.intersection(fr)
+# print(len(result))

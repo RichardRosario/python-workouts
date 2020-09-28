c65697a3c11590b562ea276adc2a20e52dcc4ede
Richard Rosario <tech2js@gmail.com>
2020-09-28 18:47:22 +0800

    more workout on datatypes

--- practice2.py
+++ practice2.py
@@ -282,4 +282,8 @@ from collections import OrderedDict
 #     command(*args.split())
 
 # Given an integer, , and  space-separated integers as input, create a tuple, , of those  integers. Then compute and print the result of .
-print(input() == 0 or hash(tuple(map(int, input().strip().split()))))
+# print(input() == 0 or hash(tuple(map(int, input().strip().split()))))
+# ===================================
+x, y, z, n = int(input()), int(input()), int(input()), int(input())
+print([[a, b, c] for a in range(0, x+1) for b in range(0, y+1)
+       for c in range(0, z+1) if a + b + c != n])

3b1728eb57eb2dee74d956c0feb63699b83ebc7d
Richard Rosario <tech2js@gmail.com>
2020-10-14 14:43:55 +0800

    regex workout

--- practice4.py
+++ practice4.py
@@ -7,3 +7,8 @@ regex_pattern = r""  # Do not delete 'r'.
 
 print("\n".join(re.split(regex_pattern, input())))
 # answer: [.,]+
+# ========================================
+# You are given a string S.
+# Your task is to find the first occurrence of an alphanumeric character in S (read from left to right) that has consecutive repetitions.
+m = re.search(r'([a-zA-Z0-9])\1+', input().strip())
+print(m.group(1) if m else -1)

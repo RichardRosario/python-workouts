284f5c4de652a4b57144a30d7a3e07c10ee04b84
Richard Rosario <catrosits@gmail.com>
2020-10-28 18:28:48 +0800

    working in strings

--- practice4.py
+++ practice4.py
@@ -106,17 +106,29 @@ import re
 
 # ================================
 # In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
-def count_substring(string, sub_string):
-    count = 0
-    for i in range(len(string)):
-        if string[i:].startswith(sub_string):
-            count += 1
-    return count
+# def count_substring(string, sub_string):
+#     count = 0
+#     for i in range(len(string)):
+#         if string[i:].startswith(sub_string):
+#             count += 1
+#     return count
 
+
+# if __name__ == '__main__':
+#     string = input().strip()
+#     sub_string = input().strip()
+
+#     count = count_substring(string, sub_string)
+#     print(count)
+
+# ===============================================
+# You are given a string S.
+# Your task is to find out if the string  contains: alphanumeric characters, alphabetical characters, digits, lowercase and uppercase characters.
 
 if __name__ == '__main__':
-    string = input().strip()
-    sub_string = input().strip()
-
-    count = count_substring(string, sub_string)
-    print(count)
+    S= input()
+    print(any(c.isalnum() for c in S))
+    print(any(c.isalpha() for c in S))
+    print(any(c.isdigit() for c in S))
+    print(any(c.islower() for c in S))
+    print(any(c.isupper() for c in S))

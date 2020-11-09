aa8096affde3e835ffdb345760016ba40c3a5b23
Richard Rosario <catrosits@gmail.com>
2020-11-09 23:01:58 +0800

    sorting

--- practice4.py
+++ practice4.py
@@ -178,20 +178,34 @@ import re
 # The maximum length of the extension is 3.
 
 import re
-def fun(s):
+# ==================================
+# def fun(s):
 
-  pattern = re.compile("^[\\w-]+@[0-9a-zA-Z]+\\.[a-z]{1,3}$")
-  return pattern.match(s)
+#   pattern = re.compile("^[\\w-]+@[0-9a-zA-Z]+\\.[a-z]{1,3}$")
+#   return pattern.match(s)
 
-def filter_mail(emails):
-    return list(filter(fun, emails))
+# def filter_mail(emails):
+#     return list(filter(fun, emails))
 
-if __name__ == '__main__':
-    n = int(input())
-    emails = []
-    for _ in range(n):
-        emails.append(input())
+# if __name__ == '__main__':
+#     n = int(input())
+#     emails = []
+#     for _ in range(n):
+#         emails.append(input())
 
-filtered_emails = filter_mail(emails)
-filtered_emails.sort()
-print(filtered_emails)
+# filtered_emails = filter_mail(emails)
+# filtered_emails.sort()
+# print(filtered_emails)
+
+# ====================================
+
+# You are given a string S.
+# S contains alphanumeric characters only.
+#  Your task is to sort the string S in the following manner:
+
+# All sorted lowercase letters are ahead of uppercase letters.
+# All sorted uppercase letters are ahead of digits.
+# All sorted odd digits are ahead of sorted even digits.
+
+
+print(*(sorted(input(), key=lambda x: (x.isdigit(), x.isdigit() and int(x)%2==0, x.isupper(), x.islower(), x))), sep='')
